4f01574f5646dd52f19bcf906982c0d6147d2ccc
Andre Hoarau <andgus95@gmail.com>
2026-02-02 09:16:41 +1300

    continuing on the lab

--- labs/lab2_trains.py
+++ labs/lab2_trains.py
@@ -9,10 +9,29 @@ page = requests.get(url)
 doc= parseString(page.content)
 '''print(doc.toprettyxml())'''
 
-objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
-for objTrainPositionsNode in objTrainPositionsNodes:
-    traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
-    traincode = traincodenode.firstChild.nodeValue.strip()
-    trainlatnode = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
-    trainlat = trainlatnode.firstChild.nodeValue.strip()
-    print (trainlat)
+retrieveTags=['TrainStatus',
+'TrainLatitude',
+'TrainLongitude',
+'TrainCode',
+'TrainDate',
+'PublicMessage',
+'Direction'
+]
+with open('week03_train.csv', mode='w', newline='') as train_file:
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    objTrainPositionsNodes = doc.getElementsByTagName('objTrainPositions')
+    for objTrainPositionsNode in objTrainPositionsNodes:
+        traincodenode = objTrainPositionsNode.getElementsByTagName(retrieveTags).item(0)
+        '''traincode = traincodenode.firstChild.nodeValue.strip()
+        trainlatnode = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
+        trainlat = trainlatnode.firstChild.nodeValue.strip()
+        dataList = []
+        dataList.append(traincode)
+        train_writer.writerow(dataList)'''
+        datalist = []
+        for retieveTag in retrieveTags:
+            datanode = objTrainPositionsNode.getElementsByTagName(retieveTag).item(0)
+            datalist.append(datanode.firstChild.nodeValue.strip())
+        train_writer.writerow(datalist)
+
