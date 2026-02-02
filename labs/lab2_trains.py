0a3e6b8197b571856bbf46e4a10a1a00058bc0bc
Andre Hoarau <andgus95@gmail.com>
2026-02-02 13:52:39 +1300

    tidying up but not enoughttrain info at the moment

--- labs/lab2_trains.py
+++ labs/lab2_trains.py
@@ -21,17 +21,39 @@ with open('week03_train.csv', mode='w', newline='') as train_file:
     train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     objTrainPositionsNodes = doc.getElementsByTagName('objTrainPositions')
+
     for objTrainPositionsNode in objTrainPositionsNodes:
-        traincodenode = objTrainPositionsNode.getElementsByTagName(retrieveTags).item(0)
+
+        # Get TrainCode properly (NOT using retrieveTags)
+        traincodenode = objTrainPositionsNode.getElementsByTagName('TrainCode').item(0)
+
+        if traincodenode and traincodenode.firstChild:
+            traincode = traincodenode.firstChild.nodeValue.strip()
+        else:
+            traincode = ""
+
         '''traincode = traincodenode.firstChild.nodeValue.strip()
         trainlatnode = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
         trainlat = trainlatnode.firstChild.nodeValue.strip()
         dataList = []
         dataList.append(traincode)
         train_writer.writerow(dataList)'''
-        datalist = []
-        for retieveTag in retrieveTags:
-            datanode = objTrainPositionsNode.getElementsByTagName(retieveTag).item(0)
-            datalist.append(datanode.firstChild.nodeValue.strip())
-        train_writer.writerow(datalist)
 
+        datalist = []
+
+        # Keep your filter, but make it safe
+        if traincode.startswith('D'):  
+
+            for retieveTag in retrieveTags:
+
+                datanode = objTrainPositionsNode.getElementsByTagName(retieveTag).item(0)
+
+                # Some XML fields are empty → avoid crashes
+                if datanode and datanode.firstChild:
+                    value = datanode.firstChild.nodeValue.strip()
+                else:
+                    value = ""
+
+                datalist.append(value)
+
+            train_writer.writerow(datalist)
