fedab9c3bb0f73065e9716ce57765744fcd213df
Andre Hoarau <andgus95@gmail.com>
2026-02-02 15:47:05 +1300

    MVP done

--- assignments/assignment2-carddraw.py
+++ assignments/assignment2-carddraw.py
@@ -2,7 +2,7 @@
 # Author: Andre Hoarau
 # Import requests to interact with the API and minidom to parse and interact with the XMLs
 import requests
-from xml.dom import minidom
+from collections import counter
 # Url for shuffling
 url_shuffle = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 # As per the documentation a get should shuffle the deck
@@ -16,9 +16,29 @@ print(deck_id)
 print(doc)
 # So now we have the deck id of our shuffled deck. 
 url_deal = 'https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count=5'
+# The above url deals the cards so we insert our shuffled deck.
 deck_deal = requests.get(url_deal)
 deal = deck_deal.json()
 cards = deal['cards']
+# Setting up lists for the values and suits to explore looking at card combinations.
+values = []
+suits = []
+# Then looking at each card in turn we can print their value and suit.
 for card in cards:
     print(f'{card['value']} of {card['suit']}')
-
+    values.append(card['value'])
+    suits.append(card['suit'])
+# As the face cards can also be part of straight we need to assign a numeric value to them for simplicity Ace will just be the highest card.
+face_card_values = {
+    "ACE": 14,
+    "KING": 13,
+    "QUEEN": 12,
+    "JACK": 11
+}
+values_assigned =[]
+for v in values:
+    if v.isdigit():
+        values_assigned.append(v)
+    else:
+        values_assigned.append(face_card_values[v])
+print(values_assigned)
