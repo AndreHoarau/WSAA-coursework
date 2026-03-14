c0c1ba113cddc9113d6a61f479700745f8f64bed
Andre Hoarau <andgus95@gmail.com>
2026-03-15 12:12:31 +1300

    improving card deal

--- assignments/assignment2-carddraw.py
+++ assignments/assignment2-carddraw.py
@@ -1,15 +1,15 @@
 # Using an api that simulates a deck of cards shuffle and deal 5 cards
 # Author: Andre Hoarau
-# Import requests to interact with the API and minidom to parse and interact with the XMLs
+# Import requests to interact with the API import counter to count the doubles and triples 
+#Counter documentation = https://docs.python.org/3/library/collections.html#collections.Counter
 import requests
-from collections import counter
+from collections import Counter
 # Url for shuffling
 url_shuffle = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-# As per the documentation a get should shuffle the deck
+# As per the documentation a get should shuffle the deck https://www.deckofcardsapi.com/
 shuffle_deck = requests.get(url_shuffle)
 print(shuffle_deck)
-# This gives a response code of 200 which is good it means that it works and we can parse the xml.
-# The response is not an XML this time but a JSON
+# This gives a response code of 200 which is good. The response is a JSON.
 doc = shuffle_deck.json()
 deck_id = doc['deck_id']
 print(deck_id)
@@ -38,7 +38,26 @@ face_card_values = {
 values_assigned =[]
 for v in values:
     if v.isdigit():
-        values_assigned.append(v)
+        values_assigned.append(int(v))
     else:
         values_assigned.append(face_card_values[v])
 print(values_assigned)
+
+# Check for pairs / triples
+counts = Counter(values_assigned)
+pairs = [v for v, cnt in counts.items() if cnt == 2]
+triples = [v for v, cnt in counts.items() if cnt == 3]
+
+if triples:
+    print("You have a triple! Congratulations!")
+elif pairs:
+    print("You have a pair! Congratulations!")
+
+# Check for straight
+sorted_values = sorted(values_assigned)
+if sorted_values == list(range(min(sorted_values), min(sorted_values) + 5)):
+    print("You have a straight! Congratulations!")
+
+# Check for flush
+if len(set(suits)) == 1:
+    print("You have a flush! All cards of the same suit! Congratulations!")
